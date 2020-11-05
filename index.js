41d6679fa078596e08a454a8088f64a742ccb924
teneons <teneons@gmail.com>
2020-11-05 23:47:18 +0200

    remake request code; make basic set data

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 
-const pokemonRequest = () => {
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=42')    //get data
-  .then(d => d.json())
-  .then(pokiData => console.log(pokiData.results))
-  .catch(() => {     //error
+async function generalPokemonsReq () {
+  try {
+    const req = await fetch('https://pokeapi.co/api/v2/pokemon?limit=42')    //get data
+    const jsonData = await req.json();
+    return await setGeneralData(jsonData);
+
+  } catch(e) {     //error
     
+    //create html message
     const alertErr = document.createElement('div');
     alertErr.className = "alert alert-danger d-flex justify-content-center";
     alertErr.setAttribute("role", "alert");
     alertErr.textContent = "Something went wrong😬";
 
-    return (
-      document.getElementById('header').append(alertErr)
-    )
-  })
+    return document.getElementById('header').append(alertErr);
+  }
 }
 
-pokemonRequest()
+//let generalData = generalPokemonsReq()
+
+const setGeneralData = (generalD) => {
+  document.querySelector('.card-title').innerHTML = generalD.results[0].name;
+  document.querySelector
+}
+
+ window.onload = generalPokemonsReq();
